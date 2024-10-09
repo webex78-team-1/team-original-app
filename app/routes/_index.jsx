33c892b2eeb24f7b04661bfd8ab57a8b26529e7c
same04 <sksamebu68@gmail.com>
2024-10-09 13:54:04 +0900

    pushしたときの状態にに戻した

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,7 +1,5 @@
 import { TextUI } from "../components/textUI.jsx";
-import { UserInfo, SignOutButton, SignInButton } from "../components/Sign.jsx";
-import { auth } from "../firebase.js";
-import { useAuthState } from "react-firebase-hooks/auth";
+import Sign from "../components/Sign.jsx";
 
 export const meta = () => {
   return [
@@ -11,40 +9,27 @@ export const meta = () => {
 };
 
 export default function Index() {
-  //ユーザーのログイン状態を管理する変数の宣言
-  const [user] = useAuthState(auth);
   return (
-    // <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-    //   <h1>Welcome to Remix (SPA Mode)</h1>
-    //   <ul>
-    //     <li>
-    //       <a
-    //         target="_blank"
-    //         href="https://remix.run/guides/spa-mode"
-    //         rel="noreferrer"
-    //       >
-    //         SPA Mode Guide
-    //       </a>
-    //     </li>
-    //     <li>
-    //       <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-    //         Remix Docs
-    //       </a>
-    //     </li>
-    //   </ul>
-    //   <Sign />
-    //   <TextUI />
-    // </div>
-    <div>
-      {user ? (
-        <div>
-          <UserInfo />
-          <SignOutButton />
-          <TextUI />
-        </div>
-      ) : (
-        <SignInButton />
-      )}
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>Welcome to Remix (SPA Mode)</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/guides/spa-mode"
+            rel="noreferrer"
+          >
+            SPA Mode Guide
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+      <Sign />
+      <TextUI />
     </div>
   );
 }
