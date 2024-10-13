641ae9b05d4e1b47a7918bbfef3ba77ff56f86ea
Tomohiro-Taniguchi <m241731@hiroshima-u.ac.jp>
2024-10-14 04:20:09 +0900

    Google Mapを活用した最適スポット検索ページを追加しました

---
+++ app/routes/search.jsx
@@ -0,0 +1,11 @@
+import { SearchMapComponent } from "../components/SearchMap.jsx";
+
+export default function SearchMap() {
+  return (
+    <div>
+      <h1>KOTABI ~孤旅~</h1>
+      <h2>Googleマップを活用した最適スポット検索</h2>
+      <SearchMapComponent />
+    </div>
+  );
+}
